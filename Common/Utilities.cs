27e8812340a4bcb94f5e3e356d94b2ef9e815d63
Chengming <v-cruan@microsoft.com>
2023-08-09 16:52:17 +0800

    update

--- Common/Utilities.cs
+++ Common/Utilities.cs
@@ -5,19 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Diagnostics;
-using System.IO;
-using Newtonsoft.Json.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Net.Http.Headers;
-using Renci.SshNet;
 
 namespace Azure.ResourceManager.Samples.Common
 {
     public static class Utilities
     {
-        public static bool IsRunningMocked { get; set; }
         public static Action<string> LoggerMethod { get; set; }
         public static Func<string> PauseMethod { get; set; }
         public static string ProjectPath { get; set; }
@@ -52,292 +44,37 @@ namespace Azure.ResourceManager.Samples.Common
             Utilities.Log("");
         }
 
-        public static string ReadLine()
-        {
-            return PauseMethod.Invoke();
-        }
+        public static string ReadLine() => PauseMethod.Invoke();
 
-        public static string CreateRandomName(string namePrefix)
-        {
-            return $"{namePrefix}{_random.Next(9999)}";
-        }
+        public static string CreateRandomName(string namePrefix) => $"{namePrefix}{_random.Next(9999)}";
 
-        public static string CreatePassword()
-        {
-            return "azure12345QWE!";
-        }
+        public static string CreatePassword() => "azure12345QWE!";
 
-        public static string CreateUsername()
-        {
-            return "tirekicker";
-        }
+        public static string CreateUsername() => "tirekicker";
 
-        private static string FormatDictionary(IReadOnlyDictionary<string, string> dictionary)
-        {
-            if (dictionary == null)
-            {
-                return string.Empty;
-            }
-
-            var outputString = new StringBuilder();
-
-            foreach (var entity in dictionary)
-            {
-                outputString.AppendLine($"{entity.Key}: {entity.Value}");
-            }
-
-            return outputString.ToString();
-        }
-
-        private static string FormatCollection(IEnumerable<string> collection)
-        {
-            return string.Join(", ", collection);
-        }
-
-        /**
-         * Retrieve the secondary service principal client ID.
-         * @param envSecondaryServicePrincipal an Azure Container Registry
-         * @return a service principal client ID
-         */
-        public static string GetSecondaryServicePrincipalClientID(string envSecondaryServicePrincipal)
-        {
-            string clientId = "";
-            File.ReadAllLines(envSecondaryServicePrincipal).All(line =>
-            {
-                var keyVal = line.Trim().Split(new char[] { '=' }, 2);
-                if (keyVal.Length < 2)
-                    return true; // Ignore lines that don't look like $$$=$$$
-                if (keyVal[0].Equals("client"))
-                    clientId = keyVal[1];
-                return true;
-            });
-
-            return clientId;
-        }
-
-        /**
-         * Retrieve the secondary service principal secret.
-         * @param envSecondaryServicePrincipal an Azure Container Registry
-         * @return a service principal secret
-         */
-        public static string GetSecondaryServicePrincipalSecret(string envSecondaryServicePrincipal)
-        {
-            string secret = "";
-            File.ReadAllLines(envSecondaryServicePrincipal).All(line =>
-            {
-                var keyVal = line.Trim().Split(new char[] { '=' }, 2);
-                if (keyVal.Length < 2)
-                    return true; // Ignore lines that don't look like $$$=$$$
-                if (keyVal[0].Equals("key"))
-                    secret = keyVal[1];
-                return true;
-            });
-
-            return secret;
-        }
-
-        public static void CreateCertificate(string domainName, string pfxPath, string password)
-        {
-            if (!IsRunningMocked)
-            {
-                string args = string.Format(
-                    @".\createCert.ps1 -pfxFileName {0} -pfxPassword ""{1}"" -domainName ""{2}""",
-                    pfxPath,
-                    password,
-                    domainName);
-                ProcessStartInfo info = new ProcessStartInfo("powershell", args);
-                string assetPath = Path.Combine(ProjectPath, "Asset");
-                info.WorkingDirectory = assetPath;
-                Process process = Process.Start(info);
-                process.WaitForExit();
-
-                if (process.ExitCode != 0)
-                {
-                    // call "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass" in powershell if you fail here
-
-                    Utilities.Log("powershell createCert.ps1 script failed");
-                }
-            }
-            else
-            {
-                //File.Copy(
-                //    Path.Combine(Utilities.ProjectPath, "Asset", "SampleTestCertificate.pfx"),
-                //    Path.Combine(Utilities.ProjectPath, "Asset", pfxPath),
-                //    overwrite: true);
-            }
-        }
-
-        public static void CreateCertificate(string domainName, string pfxName, string cerName, string password)
-        {
-            if (!IsRunningMocked)
-            {
-                string args = string.Format(
-                    @".\createCert1.ps1 -pfxFileName {0} -cerFileName {1} -pfxPassword ""{2}"" -domainName ""{3}""",
-                    pfxName,
-                    cerName,
-                    password,
-                    domainName);
-                ProcessStartInfo info = new ProcessStartInfo("powershell", args);
-                string assetPath = Path.Combine(ProjectPath, "Asset");
-                info.WorkingDirectory = assetPath;
-                Process.Start(info).WaitForExit();
-            }
-            else
-            {
-                //File.Copy(
-                //    Path.Combine(Utilities.ProjectPath, "Asset", "SampleTestCertificate.pfx"),
-                //    Path.Combine(Utilities.ProjectPath, "Asset", pfxName),
-                //    overwrite: true);
-            }
-        }
 
         public static string CheckAddress(string url, IDictionary<string, string> headers = null)
         {
-            if (!IsRunningMocked)
+            try
             {
-                try
+                using (var client = new HttpClient())
                 {
-                    using (var client = new HttpClient())
+                    client.Timeout = TimeSpan.FromSeconds(300);
+                    if (headers != null)
                     {
-                        client.Timeout = TimeSpan.FromSeconds(300);
-                        if (headers != null)
+                        foreach (var header in headers)
                         {
-                            foreach (var header in headers)
-                            {
-                                client.DefaultRequestHeaders.Add(header.Key, header.Value);
-                            }
-                        }
-                        return $"Ping: {url}: {client.GetAsync(url).Result.StatusCode}";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Utilities.Log(ex);
-                }
-            }
-
-            return "[Running in PlaybackMode]";
-        }
-
-        public static string PostAddress(string url, string body, IDictionary<string, string> headers = null)
-        {
-            if (!IsRunningMocked)
-            {
-                try
-                {
-                    using (var client = new HttpClient())
-                    {
-                        if (headers != null)
-                        {
-                            foreach (var header in headers)
-                            {
-                                client.DefaultRequestHeaders.Add(header.Key, header.Value);
-                            }
-                        }
-                        return client.PostAsync(url, new StringContent(body)).Result.ToString();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Utilities.Log(ex);
-                }
-            }
-
-            return "[Running in PlaybackMode]";
-        }
-
-        public static void DeprovisionAgentInLinuxVM(string host, int port, string userName, string password)
-        {
-            Utilities.Log("is mocked:" + IsRunningMocked);
-            if (!IsRunningMocked)
-            {
-                Console.WriteLine("Trying to de-provision: " + host);
-                Console.WriteLine("ssh connection status: " + TrySsh(
-                    host,
-                    port,
-                    userName,
-                    password,
-                    "sudo waagent -deprovision+user --force"));
-            }
-        }
-
-        public static string GetArmTemplate(string templateFileName)
-        {
-            var adminUsername = "tirekicker";
-            var adminPassword = CreatePassword();
-            var hostingPlanName = CreateRandomName("hpRSAT");
-            var webAppName = CreateRandomName("wnRSAT");
-            var armTemplateString = File.ReadAllText(Path.Combine(Utilities.ProjectPath, "Asset", templateFileName));
-
-            var parsedTemplate = JObject.Parse(armTemplateString);
-
-            if (String.Equals("ArmTemplate.json", templateFileName, StringComparison.OrdinalIgnoreCase))
-            {
-                parsedTemplate.SelectToken("parameters.hostingPlanName")["defaultValue"] = hostingPlanName;
-                parsedTemplate.SelectToken("parameters.webSiteName")["defaultValue"] = webAppName;
-                parsedTemplate.SelectToken("parameters.skuName")["defaultValue"] = "B1";
-                parsedTemplate.SelectToken("parameters.skuCapacity")["defaultValue"] = 1;
-            }
-            else if (String.Equals("ArmTemplateVM.json", templateFileName, StringComparison.OrdinalIgnoreCase))
-            {
-                parsedTemplate.SelectToken("parameters.adminUsername")["defaultValue"] = adminUsername;
-                parsedTemplate.SelectToken("parameters.adminPassword")["defaultValue"] = adminPassword;
-            }
-            return parsedTemplate.ToString();
-        }
-
-        public static string GetCertificatePath(string certificateName)
-        {
-            return Path.Combine(Utilities.ProjectPath, "Asset", certificateName);
-        }
-
-        private static string TrySsh(
-            string host,
-            int port,
-            string userName,
-            string password,
-            string commandToExecute)
-        {
-            string commandOutput = null;
-            var backoffTime = 30 * 1000;
-            var retryCount = 3;
-
-            while (retryCount > 0)
-            {
-                using (var sshClient = new SshClient(host, port, userName, password))
-                {
-                    try
-                    {
-                        sshClient.Connect();
-                        if (commandToExecute != null)
-                        {
-                            using (var command = sshClient.CreateCommand(commandToExecute))
-                            {
-                                commandOutput = command.Execute();
-                            }
-                        }
-                        break;
-                    }
-                    catch (Exception exception)
-                    {
-                        retryCount--;
-                        if (retryCount == 0)
-                        {
-                            throw exception;
+                            client.DefaultRequestHeaders.Add(header.Key, header.Value);
                         }
                     }
-                    finally
-                    {
-                        try
-                        {
-                            sshClient.Disconnect();
-                        }
-                        catch { }
-                    }
+                    return $"Ping: {url}: {client.GetAsync(url).Result.StatusCode}";
                 }
             }
-
-            return commandOutput;
+            catch (Exception ex)
+            {
+                Utilities.Log(ex);
+                return ex.ToString();
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,12 +11,6 @@ using Azure.ResourceManager.Cdn.Models;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Resources.Models;
 using Azure.ResourceManager.Samples.Common;
-using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Reflection;
 
 namespace ManageCdn
 {
@@ -56,19 +50,19 @@ namespace ManageCdn
                 websites.Add(await CreateWebApp(resourceGroup, AzureLocation.EastUS));
                 websites.Add(await CreateWebApp(resourceGroup, AzureLocation.WestUS));
 
-                //// 2 in EU
-                //websites.Add(await CreateWebApp(resourceGroup, AzureLocation.NorthEurope));
-                //websites.Add(await CreateWebApp(resourceGroup, AzureLocation.WestEurope));
+                //2 in EU
+                websites.Add(await CreateWebApp(resourceGroup, AzureLocation.NorthEurope));
+                websites.Add(await CreateWebApp(resourceGroup, AzureLocation.WestEurope));
 
-                //// 2 in Southeast
-                //await CreateWebApp(resourceGroup, AzureLocation.EastAsia);
-                //await CreateWebApp(resourceGroup, AzureLocation.SoutheastAsia);
+                // 2 in Southeast
+                websites.Add(await CreateWebApp(resourceGroup, AzureLocation.EastAsia));
+                websites.Add(await CreateWebApp(resourceGroup, AzureLocation.SoutheastAsia));
 
-                //// 1 in Brazil
-                //await CreateWebApp(resourceGroup, AzureLocation.BrazilSouth);
+                // 1 in Brazil
+                websites.Add(await CreateWebApp(resourceGroup, AzureLocation.BrazilSouth));
 
-                //// 1 in Japan
-                //await CreateWebApp(resourceGroup, AzureLocation.JapanWest);
+                // 1 in Japan
+                websites.Add(await CreateWebApp(resourceGroup, AzureLocation.JapanWest));
 
                 // =======================================================================================
                 // Create CDN profile using Standard Verizon SKU with endpoints in each region of Web apps.
@@ -76,7 +70,6 @@ namespace ManageCdn
                 string afdProfileName = Utilities.CreateRandomName("AFDProfile");
                 ProfileData afdProfileInput = new ProfileData("Global", new CdnSku { Name = CdnSkuName.PremiumAzureFrontDoor })
                 {
-                    OriginResponseTimeoutSeconds = 60
                 };
                 var afdProfileLro = await resourceGroup.GetProfiles().CreateOrUpdateAsync(WaitUntil.Completed, afdProfileName, afdProfileInput);
                 ProfileResource afdProfile = afdProfileLro.Value;
@@ -85,27 +78,30 @@ namespace ManageCdn
                 // CreateAfdEndpoint
                 Utilities.Log($"Creating a FrontDoor endpoint..");
                 string afdEndpointName = Utilities.CreateRandomName("afdtestendpoint");
-                FrontDoorEndpointData input = new FrontDoorEndpointData(AzureLocation.WestUS);
+                FrontDoorEndpointData input = new FrontDoorEndpointData(AzureLocation.WestUS)
+                {
+                    EnabledState = EnabledState.Enabled,
+                };
                 var afdEndpointLro = await afdProfile.GetFrontDoorEndpoints().CreateOrUpdateAsync(WaitUntil.Completed, afdEndpointName, input);
                 FrontDoorEndpointResource afdEndpointInstance = afdEndpointLro.Value;
 
                 // CreateAfdOriginGroup
-                Utilities.Log($"Creating a origin group..");
+                Utilities.Log($"Creating an origin group..");
                 string afdOriginGroupName = Utilities.CreateRandomName("AfdOriginGroup");
                 FrontDoorOriginGroupData afdOriginGroupInput = new FrontDoorOriginGroupData
                 {
                     HealthProbeSettings = new HealthProbeSettings
                     {
-                        ProbePath = "/healthz",
+                        ProbePath = "/",
+                        ProbeProtocol = HealthProbeProtocol.Http,
                         ProbeRequestType = HealthProbeRequestType.Head,
-                        ProbeProtocol = HealthProbeProtocol.Https,
-                        ProbeIntervalInSeconds = 60
+                        ProbeIntervalInSeconds = 100
                     },
                     LoadBalancingSettings = new LoadBalancingSettings
                     {
-                        SampleSize = 5,
-                        SuccessfulSamplesRequired = 4,
-                        AdditionalLatencyInMilliseconds = 200
+                        SampleSize = 4,
+                        SuccessfulSamplesRequired = 3,
+                        AdditionalLatencyInMilliseconds = 50
                     }
                 };
                 var afdOriginGroupLro = await afdProfile.GetFrontDoorOriginGroups().CreateOrUpdateAsync(WaitUntil.Completed, afdOriginGroupName, afdOriginGroupInput);
@@ -119,19 +115,16 @@ namespace ManageCdn
                     FrontDoorOriginData afdOriginInput = new FrontDoorOriginData
                     {
                         HostName = website.Data.DefaultHostName,
+                        OriginHostHeader = website.Data.DefaultHostName,
+                        HttpPort  = 80,
+                        HttpsPort = 443,
                         Priority = 1,
-                        Weight = 1000
+                        Weight = 1000,
+                        EnabledState = EnabledState.Enabled,
                     };
                     var afdOriginLro = await afdOriginGroup.GetFrontDoorOrigins().CreateOrUpdateAsync(WaitUntil.Completed, afdOriginName, afdOriginInput);
                     FrontDoorOriginResource afdOrigin = afdOriginLro.Value;
                 }
-
-
-                //CreateAfdRuleSet
-                Utilities.Log($"Creating a rule set");
-                string afdRuleSetName = Utilities.CreateRandomName("AfdRuleSet");
-                var afdRuleSetLro = await afdProfile.GetFrontDoorRuleSets().CreateOrUpdateAsync(WaitUntil.Completed, afdRuleSetName);
-                FrontDoorRuleSetResource afdRuleSet = afdRuleSetLro.Value;
 
                 //CreateAfdRoute
                 Utilities.Log($"Creating a route");
@@ -141,60 +134,15 @@ namespace ManageCdn
                     OriginGroupId = afdOriginGroup.Id,
                     LinkToDefaultDomain = LinkToDefaultDomain.Enabled,
                     EnabledState = EnabledState.Enabled,
-                    RuleSets =
-                        {
-                            new WritableSubResource()
-                            {
-                                Id = afdRuleSet.Id
-                            }
-                        },
-                    PatternsToMatch = { "/*" }
+                    PatternsToMatch = { "/*" },
+                    ForwardingProtocol  = ForwardingProtocol.MatchRequest,
+                    HttpsRedirect = HttpsRedirect.Enabled
                 };
                 var afdRouteLro = await afdEndpointInstance.GetFrontDoorRoutes().CreateOrUpdateAsync(WaitUntil.Completed, afdRouteName, afdRouteDataInput);
                 FrontDoorRouteResource afdRoute = afdRouteLro.Value;
 
-
-
-                //// =======================================================================================
-                //// Create CDN profile using Standard Verizon SKU with endpoints in each region of Web apps.
-                //Utilities.Log("Creating a CDN Profile");
-
-                //// create Cdn Profile definition object that will let us do a for loop
-                //// to define all 8 endpoints and then parallelize their creation
-                //var profileDefinition = azure.CdnProfiles.Define(cdnProfileName)
-                //        .WithRegion(Region.USSouthCentral)
-                //        .WithExistingResourceGroup(rgName)
-                //        .WithStandardVerizonSku();
-
-                //// define all the endpoints. We need to keep track of the last creatable stage
-                //// to be able to call create on the entire Cdn profile deployment definition.
-                //ICreatable<ICdnProfile> cdnCreatable = null;
-                //foreach (var webSite in appNames)
-                //{
-                //    cdnCreatable = profileDefinition
-                //            .DefineNewEndpoint()
-                //                .WithOrigin(webSite + Suffix)
-                //                .WithHostHeader(webSite + Suffix)
-                //                .WithCompressionEnabled(true)
-                //                .WithContentTypeToCompress("application/javascript")
-                //                .WithQueryStringCachingBehavior(QueryStringCachingBehavior.IgnoreQueryString)
-                //            .Attach();
-                //}
-
-                //// create profile and then all the defined endpoints in parallel
-                //ICdnProfile profile = cdnCreatable.Create();
-
-                //// =======================================================================================
-                //// Load some content (referenced by Web Apps) to the CDN endpoints.
-                //var contentToLoad = new HashSet<string>();
-                //contentToLoad.Add("/server.js");
-                //contentToLoad.Add("/pictures/microsoft_logo.png");
-
-                //foreach (ICdnEndpoint endpoint in profile.Endpoints.Values)
-                //{
-                //    endpoint.LoadContent(contentToLoad);
-                //}
-
+                Utilities.Log("Usually, deploying Azure Front Door takes a few minutes.");
+                Utilities.Log($"After the AFD deployment is complete, you can browse {afdRoute.Data.EndpointName} to verify.");
             }
             finally
             {
@@ -217,7 +165,6 @@ namespace ManageCdn
             {
                 //=================================================================
                 // Authenticate
-                //var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
                 var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
                 var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
                 var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
@@ -240,37 +187,14 @@ namespace ManageCdn
         {
             string appNamePrefix = "sampletestwebapp";
             var appName = Utilities.CreateRandomName(appNamePrefix);
-
-            Utilities.Log($"Creating {location} web app {appName} with master branch...");
+            Utilities.Log($"Creating {location} web app: {appName}...");
 
             WebSiteCollection collection = resourceGroup.GetWebSites();
-            var data = new WebSiteData(location)
-            {
-                /*Reserved = false,
-                IsXenon = false,
-                HyperV = false,
-                SiteConfig = new SiteConfig
-                {
-                    NetFrameworkVersion = "v4.6",
-                    AppSettings =
-                    {
-                        new NameValuePair
-                        {
-                            Name = "WEBSITE_NODE_DEFAULT_VERSION",
-                            Value = "10.14"
-                        }
-                    },
-                    LocalMySqlEnabled = false,
-                    Http20Enabled = true
-                },
-                ScmSiteAlsoStopped = false,
-                HttpsOnly = false*/
-            };
+            WebSiteData data = new WebSiteData(location){};
             ArmOperation<WebSiteResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, appName, data);
             WebSiteResource app = lro.Value;
 
             Utilities.Log("Created web app " + app.Data.Name);
-
             Utilities.Log("CURLing " + app.Data.DefaultHostName + "...");
             Utilities.Log(Utilities.CheckAddress("http://" + app.Data.DefaultHostName));
             return app;
